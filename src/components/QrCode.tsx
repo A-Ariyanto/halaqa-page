a69c1dce5462e9b78019da09af65464ef488fecb
Abdullah Ariyanto <z5543164@ad.unsw.edu.au>
2025-05-30 14:32:55 +1000

    Fixed QrCode component and improve it

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,20 +1,30 @@
 import Image from 'next/image'
 
 import qrCodes from '@/images/qr-codes.png'
+import ISOC_halaqa_group from '@/images/ISOC-halaqa-qr-code.png'
+import YOSH_circle_group from '@/images/YOSH-qr-code.png'
 
 export function QrCode() {
   return (
     <section id="qrCode" aria-label="QRCode" className="py-20">
-       <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-orange-900 sm:text-5xl">
-          Please join our Whatsapp group for updates and announcements.
-        </h2>
-      <div className="flex justify-center items-center min-h-[16rem]">
+      <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-orange-900 sm:text-5xl">
+        Join our Whatsapp group for updates and announcements
+      </h2>
+      <div className="mx-auto flex min-h-[16rem] max-w-3xl flex-col items-center justify-center gap-8 sm:flex-row">
         <Image
-          src={qrCodes}
-          alt="QR Codes"
+          src={ISOC_halaqa_group}
+          alt="ISOC Halaqa QR Code"
           width={1300}
           height={1300}
-          className="object-contain"
+          className="h-128 w-128 object-contain"
+          unoptimized
+        />
+        <Image
+          src={YOSH_circle_group}
+          alt="YOSH Circle QR Code"
+          width={1300}
+          height={1300}
+          className="h-128 w-128 object-contain"
           unoptimized
         />
       </div>
